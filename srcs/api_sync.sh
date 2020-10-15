377b1085fdb38744599bc965de9adc98df9d27b6
sancho <sancho@student.42seoul.kr>
2020-10-15 23:06:02 +0900

    refresh 20s

--- srcs/api_sync.sh
+++ srcs/api_sync.sh
@@ -1,1 +1,3 @@
-/app/java-se-8u41-ri/bin/java -cp /app/api:/app/java-se-8u41-ri/lib/mariadb-java-client-2.7.0.jar ApiExplorer >> /app/api/log.txt
+(sleep 1 && /app/java-se-8u41-ri/bin/java -cp /app/api:/app/java-se-8u41-ri/lib/mariadb-java-client-2.7.0.jar ApiExplorer >> /app/api/log.txt) &
+(sleep 20 && /app/java-se-8u41-ri/bin/java -cp /app/api:/app/java-se-8u41-ri/lib/mariadb-java-client-2.7.0.jar ApiExplorer >> /app/api/log.txt) &
+(sleep 40 && /app/java-se-8u41-ri/bin/java -cp /app/api:/app/java-se-8u41-ri/lib/mariadb-java-client-2.7.0.jar ApiExplorer >> /app/api/log.txt)
